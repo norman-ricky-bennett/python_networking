02a72eb3279aac17879ba7f13177d11bb7a72e33
Ricky <norman.bennett525@gmail.com>
2022-03-20 10:09:17 -0400

    python

---
+++ remote_ip.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# Find IP address of remote host 
+
+import socket
+
+def get_remote_machine_info():
+    # Change this URL for any address you would like to query
+    remote_host = 'www.python.org'
+    try:
+        print("The IP address is: %s"
+        %socket.gethostbyname(remote_host))
+    except socket.error as err_msg:
+        print("%s: %s" %(remote_host, err_msg))
+
+if __name__ == '__main__':
+    get_remote_machine_info()
